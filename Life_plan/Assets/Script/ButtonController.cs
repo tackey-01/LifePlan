e69aef528734b7359e233abef066b8f6b6077ec2
SHIN02\takinoshin <shin1989takino@gmail.com>
2018-07-10 19:23:33 +0900

    【開発】アイテム、図鑑、履歴の画面作成

--- Life_plan/Assets/Script/ButtonController.cs
+++ Life_plan/Assets/Script/ButtonController.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour
 {
     public CanvasGroup mainCanvas;
+    private GameObject Gameobj;
+    public string flagName;
+
     public void LoadSetting()
     {
         GameObject.Find("Canvas").transform.Find("ContractPanel").gameObject.SetActive(true);
@@ -23,4 +26,51 @@ public class ButtonController : MonoBehaviour
     {
         SceneManager.LoadScene("TitleScene");
     }
+
+    public void MenuListTrue()
+    {
+        GameObject.Find("Canvas").transform.Find("CharacterCanvas").transform.Find("MenuPanel").gameObject.SetActive(true);
+        GameObject.Find("Canvas").transform.Find("CharacterCanvas").transform.Find("Menubutton").gameObject.SetActive(false);
+    }
+
+    public void ListUp()
+    {
+        if (flagName == null)
+        {
+            return;
+        }
+
+        Gameobj = GameObject.Find("Canvas").transform.Find(flagName).gameObject;
+
+        if (Gameobj == null)
+        {
+            return;
+        }
+        GameObject.Find("Canvas").transform.Find("CharacterCanvas").transform.Find("MenuPanel").gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.Find("CharacterCanvas").transform.Find("Menubutton").gameObject.SetActive(true);
+        Gameobj.SetActive(true);
+        // Canvas lock
+        mainCanvas = GameObject.Find("Canvas").gameObject.GetComponent<CanvasGroup>();
+        mainCanvas.interactable = false;
+    }
+
+    public void ListClose()
+    {
+        if(flagName == null)
+        {
+            return;
+        }
+
+        Gameobj = GameObject.Find("Canvas").transform.Find(flagName).gameObject;
+
+        if (Gameobj == null)
+        {
+            return;
+        }
+
+        Gameobj.SetActive(false);
+        mainCanvas = GameObject.Find("Canvas").gameObject.GetComponent<CanvasGroup>();
+        mainCanvas.interactable = true;
+    }
+    
 }
